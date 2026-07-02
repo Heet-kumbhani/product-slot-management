d31917d873eeffe46a089a05d55b190f021c8505
Heet Kumbhani <heetp821@gmail.com>
2026-07-02 23:38:36 +0530

    E-commerce price fillter

--- Spam_email.py
+++ Spam_email.py
@@ -1,25 +1,19 @@
-# Number of blacklisted sender IDs
 n = int(input("Enter number of sender IDs: "))
 
-# Input blacklist
 blacklist = []
 
 print("Enter blacklisted sender IDs:")
 for i in range(n):
     blacklist.append(input())
 
-# Incoming sender ID
 sender = input("Enter incoming sender ID: ")
 
-# Linear Search
 found = False
 
 for i in range(n):
     if blacklist[i] == sender:
         found = True
         break
-
-# Display result
 if found:
     print("Spam Detected!")
 else:
